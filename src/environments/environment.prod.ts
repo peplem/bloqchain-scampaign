01c2bbe14c05460fa0ff76f4228134c7b99c059b
TestDotCom <calliari.alessio@gmail.com>
2022-02-16 15:40:20 +0100

    remove old file

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  truffleUrl: 'ws://localhost:9545',
-  userAddress: '0x704109b0fed58e84f890d0fbd084b66842dcde14',
-  crowdFundingAddress: '0xAbE843Dd143A9cf72775B2b0645E0Cd9887e05b4',
-  RegistryAddress: '0xe01a9E15D5AfEA177ea3bEF31994761477B9d80d',
-};
